963da607584f54c09b5886813207371476446714
luisgiraldo0420 <luis.giraldo@gradiweb.com>
2022-04-28 19:15:28 -0500

    .

--- assets/productsgrid.js
+++ assets/productsgrid.js
@@ -1,49 +1,66 @@
-
-
-  const enviar = (id) =>{
-    console.log('llega');
-  let formData = {
-   'items': [{
-    'id': id,
-    'quantity': 2
-    }]
-  };
-  fetch(window.Shopify.routes.root + 'cart/add.js', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  })
-  .then(response => {
-    return response.json();
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  })
-  .finally(()=>{
-    window.document.location.href = "https://luis-giraldo-gradi-store.myshopify.com/cart"
-  })
-  }
-  
-  
-     let inputs = document.getElementsByClassName("input_productgrid")
-      for(item of inputs) {
-          item.addEventListener("mouseover", function(e) {
-              let image = e.target.getAttribute("image_hover")
-              let imageProduct = document.getElementById(e.target.getAttribute("idImageProd"))
-              imageProduct.setAttribute("src", image)
-          })
-          item.addEventListener("mouseout", function(e) {
-              let imageProduct = document.getElementById(e.target.getAttribute("idImageProd"))
-              let image = imageProduct.getAttribute("defaultImage")
-              imageProduct.setAttribute("src", image)
-          })
-          item.addEventListener("click", function(e) {
-              let image = e.target.getAttribute("image_hover")
-              let imageProduct = document.getElementById(e.target.getAttribute("idImageProd"))
-              imageProduct.setAttribute("src", image);
-              imageProduct.setAttribute("defaultImage", image);
-          })
-      }
-  
+let products={
+    id:undefined,
+    quantity:1,
+};
+const updateProduct=(variant)=>{
+    console.log(variant, '---------');
+    products.id=variant.id
+}
+const updateQuantity=(quantity)=>{
+    products.quantity=quantity;
+}
+const addToCart= (button) => {
+    const quantity=document.getElementById(`quantity__input-${button.id}`)
+    console.log('-----------------', quantity);
+    updateQuantity(quantity.value);
+    if(products.id!==undefined){
+        fetch(window.Shopify.routes.root + 'cart/add.js', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(products)
+        })
+        .then(response => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        })
+        .finally(() => {
+            products={
+                id:undefined,
+                quantity:1,
+            };
+            window.document.location.href = "https://luis-giraldo-store.myshopify.com/cart"
+        });
+    }
+}
+const buttonsAdd=document.getElementsByClassName('button_productgrid')
+    for(button of buttonsAdd){
+        button.addEventListener('click', addToCart)
+    }
+let inputs = document.getElementsByClassName("input_productgrid")
+for(item of inputs) {
+    item.addEventListener("mouseover", function(e) {
+        let image = e.target.getAttribute("image_hover")
+        let imageProduct = document.getElementById(e.target.getAttribute("idImageProd"))
+        imageProduct.setAttribute("src", image)
+    })
+    item.addEventListener("mouseout", function(e) {
+        let imageProduct = document.getElementById(e.target.getAttribute("idImageProd"))
+        let image = imageProduct.getAttribute("defaultImage")
+        imageProduct.setAttribute("src", image)
+    })
+    item.addEventListener("click", function(e) {
+        let image = e.target.getAttribute("image_hover")
+        let imageProduct = document.getElementById(e.target.getAttribute("idImageProd"))
+        let variant = e.target.getAttribute("variant")
+        console.log(variant);
+        let idProduct = e.target.getAttribute("idProduct")
+        let button=document.getElementById(idProduct)
+        imageProduct.setAttribute("src", image)
+        imageProduct.setAttribute("defaultImage", image)
+        button.setAttribute("currentVariant", variant)
+    })
+}
