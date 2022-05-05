5def35d821f241e553e0f4f8c2106ce5ad8a7e61
luisgiraldo0420 <luis.giraldo@gradiweb.com>
2022-05-05 11:42:29 -0500

    banner

--- assets/orders-users.js
+++ assets/orders-users.js
@@ -1,4 +1,23 @@
-fetch('https://orders-customer.herokuapp.com/orders-finish')
+
+
+function getOrders(){
+      const orders = document.querySelector('.orders_banner')
+      fetch('https://orders-customer.herokuapp.com/orders-finish')
       .then(response => response.json())
-      .then(DataOrderFinish => console.log(DataOrderFinish))
+      .then(DataOrderFinish =>{
+            orders.innerHTML = DataOrderFinish.orders.length
+      })
       .catch(error => console.log('Error', error))
+}
+function getCustomers(){
+      const customers = document.querySelector('.customers_banner')
+      fetch('https://orders-customer.herokuapp.com/customers')
+      .then(response => response.json())
+      .then(DataOrderFinish =>{
+            customers.innerHTML = DataOrderFinish.customers.length
+      })
+      .catch(error => console.log('Error', error))
+}
+
+getOrders();
+getCustomers();
